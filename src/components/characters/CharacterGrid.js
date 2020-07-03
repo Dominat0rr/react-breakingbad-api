72a3a5f9ef74d3dee96b4b0990a4c38546378ad0
Dominat0rr <claermandominik@gmail.com>
2020-07-04 01:59:21 +0200

    Finish project
    Small Front-end application using the Breakingbad API: breakingbadapi.com
    Added Grid view for all characters
    Added search box
    Added spinner gif for loading

---
+++ src/components/characters/CharacterGrid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Character from './Character';
+import Spinner from '../ui/Spinner';
+
+const CharacterGrid = ({ characters, isLoading }) => {
+    return isLoading ? (<Spinner />) : (<section className="cards">
+        {characters.map(character => (
+            <Character key={character.char_id} character={character}></Character>
+        ))}
+    </section>
+    );
+}
+
+export default CharacterGrid
